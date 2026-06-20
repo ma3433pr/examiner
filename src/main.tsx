9bfed8739b720eaa34ffd2bfadc0442cfd01b6b4
Mohammad <mohammad@Mohammads-MacBook-Air.local>
2026-06-20 19:53:57 +0400

    First release

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "@fontsource/vazirmatn";
+// import "@fontsource/vazirmatn";
 
 import "./styles/global.css";
 
